b83810c1e8cf670033697f485720e49ccb3f2be8
Josue Ham <josue.ham@unitec.edu>
2022-11-24 16:19:30 -0600

    Mas opciones agregadas, falta multiplicacion matricial

--- src/s7_matrices_josueham/S7_matrices_JosueHam.java
+++ src/s7_matrices_josueham/S7_matrices_JosueHam.java
@@ -1,5 +1,6 @@
 package s7_matrices_josueham;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
@@ -24,7 +25,9 @@ public class S7_matrices_JosueHam {
                     + "\n8 -> Suma de filas"
                     + "\n9 -> Suma de columnas"
                     + "\n10 -> Reemplazar esquinas de una columna por ceros"
-                    + "\n11 -> Salir del programa"
+                    + "\n11 -> Ordenamiento"
+                    + "\n12 -> Multiplicacion de Matrices"
+                    + "\n13 -> Salir del programa"
                     + "\nIngrese la opcion que desea: ";
             String input = JOptionPane.showInputDialog(null, menu, "Menu", 3);
             opcion = Integer.parseInt(input);
@@ -71,7 +74,7 @@ public class S7_matrices_JosueHam {
                     JOptionPane.showMessageDialog(null, "Matriz 2:\n" + imprimir(SumaParalela(matriz1, matriz2, filas, columnas)));
                 }//Fin case 3
                 break;
-                
+
                 case 4: {
                     String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
                     int filas = Integer.parseInt(fil);
@@ -81,10 +84,10 @@ public class S7_matrices_JosueHam {
                     int[][] numeros = lectura(filas, columnas);
                     JOptionPane.showMessageDialog(null, imprimir(numeros));
                     JOptionPane.showMessageDialog(null, "La suma de la primera diagonal es: " + sumaPrimerDiagonal(numeros));
-                    
+
                 }//Fin case 4
                 break;
-                
+
                 case 5: {
                     String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
                     int filas = Integer.parseInt(fil);
@@ -96,7 +99,7 @@ public class S7_matrices_JosueHam {
                     JOptionPane.showMessageDialog(null, "La suma de la segunda diagonal es: " + sumaSegundaDiagonal(numeros));
                 }//Fin case 5
                 break;
-                
+
                 case 6: {
                     String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
                     int filas = Integer.parseInt(fil);
@@ -108,7 +111,7 @@ public class S7_matrices_JosueHam {
                     JOptionPane.showMessageDialog(null, "La suma triangular superior es: " + TriangularSuperior(numeros));
                 }//Fin case 6
                 break;
-                
+
                 case 7: {
                     String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
                     int filas = Integer.parseInt(fil);
@@ -119,7 +122,7 @@ public class S7_matrices_JosueHam {
                     JOptionPane.showMessageDialog(null, imprimir(numeros));
                     JOptionPane.showMessageDialog(null, "La suma triangular inferior es: " + TriangularInferior(numeros));
                 }//Fin case 7
-                
+
                 case 8: {
                     String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
                     int filas = Integer.parseInt(fil);
@@ -128,10 +131,10 @@ public class S7_matrices_JosueHam {
 
                     int[][] numeros = lectura(filas, columnas);
                     JOptionPane.showMessageDialog(null, imprimir(numeros));
-                    JOptionPane.showMessageDialog(null, "La suma de las filas es:\n" + imprimirJOptionPane(SumaFilas(numeros,filas)));
+                    JOptionPane.showMessageDialog(null, "La suma de las filas es:\n" + imprimirJOptionPane(SumaFilas(numeros, filas)));
                 }//Fin case 8
                 break;
-                
+
                 case 9: {
                     String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
                     int filas = Integer.parseInt(fil);
@@ -140,10 +143,10 @@ public class S7_matrices_JosueHam {
 
                     int[][] numeros = lectura(filas, columnas);
                     JOptionPane.showMessageDialog(null, imprimir(numeros));
-                    JOptionPane.showMessageDialog(null, "La suma de las filas es:\n" + imprimirJOptionPane(SumaColumnas(numeros,columnas)));
+                    JOptionPane.showMessageDialog(null, "La suma de las filas es:\n" + imprimirJOptionPane(SumaColumnas(numeros, columnas)));
                 }//Fin case 9
                 break;
-                
+
                 case 10: {
                     String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
                     int filas = Integer.parseInt(fil);
@@ -153,15 +156,57 @@ public class S7_matrices_JosueHam {
                     int[][] numeros = lectura(filas, columnas);
                     JOptionPane.showMessageDialog(null, "Matriz antes del reemplazo:\n" + imprimir(numeros));
                     JOptionPane.showMessageDialog(null, "Matriz despues del reemplazo:\n" + imprimir(ReemplazarEsquinas(numeros)));
-                    
+
                 }//Fin case 10
                 break;
-                
+
+                case 11: {
+                    String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
+                    int filas = Integer.parseInt(fil);
+                    String col = JOptionPane.showInputDialog(null, "Columnas de la matriz: ", "Columnas de la matriz", 1);
+                    int columnas = Integer.parseInt(col);
+
+                    int[][] numeros = lectura(filas, columnas);
+                    JOptionPane.showMessageDialog(null, "Matriz:\n" + imprimir(numeros));
+                    JOptionPane.showMessageDialog(null, imprimir(ordenamiento(numeros)));
+                }//Fin case 11
+                break;
+
+                case 12: {
+                    String fil = JOptionPane.showInputDialog(null, "Filas de la matriz: ", "Filas de la matriz", 1);
+                    int filas = Integer.parseInt(fil);
+                    String col = JOptionPane.showInputDialog(null, "Columnas de la matriz: ", "Columnas de la matriz", 1);
+                    int columnas = Integer.parseInt(col);
+
+                    int[][] matriz1 = lectura(filas, columnas);
+                    
+                    String fil2 = JOptionPane.showInputDialog(null, "Filas de la matriz 2: ", "Filas de la matriz", 1);
+                    int filas2 = Integer.parseInt(fil2);
+                    String col2 = JOptionPane.showInputDialog(null, "Columnas de la matriz 2: ", "Columnas de la matriz", 1);
+                    int columnas2= Integer.parseInt(col2);
+                    
+                    int[][] matriz2 = lectura(filas2, columnas2);
+                    
+                    if (columnas == filas2) {
+                        JOptionPane.showMessageDialog(null, "Matriz 1:\n" + imprimir(matriz1));
+                        JOptionPane.showMessageDialog(null, "Matriz 2:\n" + imprimir(matriz2));
+                    }//Fin if
+                    else {
+                        System.out.println("filas 1: " + filas);
+                        System.out.println("colum 1: " + columnas);
+                        System.out.println("filas 2: " + filas2);
+                        System.out.println("colum 2: " + columnas2);
+                        JOptionPane.showMessageDialog(null, "Las columnas de la matriz 1 deben ser iguales a las filas de la matriz 2");
+                    }//Fin else
+
+                }//Fin case 12
+                break;
+
                 default:
                     JOptionPane.showMessageDialog(null, "Saliendo del programa...");
             }//Fin del switch
 
-        } while (opcion != 11);
+        } while (opcion != 13);
     }//Fin del main 
 
     public static int[][] lectura(int filas, int columnas) {
@@ -182,7 +227,7 @@ public class S7_matrices_JosueHam {
         }
         return resultado;
     }
-    
+
     public static String imprimir(int[][] numeros) {
         String cadena = "";
 
@@ -252,7 +297,7 @@ public class S7_matrices_JosueHam {
         }
         return respuesta;
     }//Fin metodo suma primer diagonal
-    
+
     public static int sumaSegundaDiagonal(int[][] numeros) {
         int respuesta = 0;
 
@@ -265,7 +310,7 @@ public class S7_matrices_JosueHam {
         }
         return respuesta;
     }//Fin metodo suma segunda diagonal
-    
+
     public static int TriangularSuperior(int[][] numeros) {
         int respuesta = 0;
 
@@ -278,7 +323,7 @@ public class S7_matrices_JosueHam {
         }
         return respuesta;
     }//Fin metodo suma triangular superior
-    
+
     public static int TriangularInferior(int[][] numeros) {
         int respuesta = 0;
 
@@ -291,46 +336,86 @@ public class S7_matrices_JosueHam {
         }
         return respuesta;
     }//Fin metodo suma triangular inferior
-    
-     public static int[] SumaFilas(int[][] numeros, int filas) {
-       
+
+    public static int[] SumaFilas(int[][] numeros, int filas) {
+
         int[] temporal = new int[filas];
 
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
-                    temporal[i]+= numeros[i][j];
+                temporal[i] += numeros[i][j];
             }
         }
         return temporal;
-        
-       
+
     }//Fin metodo suma de filas
-     
-     public static int[] SumaColumnas(int[][] numeros, int columnas) {
-       
+
+    public static int[] SumaColumnas(int[][] numeros, int columnas) {
+
         int[] temporal = new int[columnas];
 
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
-                    temporal[j]+= numeros[i][j];
+                temporal[j] += numeros[i][j];
             }
         }
         return temporal;
-        
-       
+
     }//Fin metodo suma de columnas
-    
-     public static int[][] ReemplazarEsquinas(int[][] numeros) {
+
+    public static int[][] ReemplazarEsquinas(int[][] numeros) {
 
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
-                   if ((i == 0 || i == numeros.length-1) && (j== 0 || j == numeros.length-1)) {
-                       numeros[i][j] = 0;
+                if ((i == 0 || i == numeros.length - 1) && (j == 0 || j == numeros.length - 1)) {
+                    numeros[i][j] = 0;
                 }
             }
         }
         return numeros;
-        
-       
+
     }//Fin metodo reemplazar esquinas
+
+    public static int[][] ordenamiento(int[][] numeros) {
+        int[][] temporal = new int[numeros.length][numeros[0].length];
+        int[] array = new int[numeros.length * numeros[0].length];
+        int cont = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[i].length; j++) {
+                array[cont] = numeros[i][j];
+                cont++;
+            }
+        }
+        array = organizar(array);
+        cont = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                temporal[i][j] = array[cont];
+                cont++;
+            }
+        }
+        String cadena = Arrays.toString(array);
+        return temporal;
+    }//Fin metodo ordenamiento array
+
+    public static int[] organizar(int[] numeros) {
+
+        int[] menores = new int[numeros.length];
+
+        for (int i = 1; i < menores.length; i++) {
+            for (int j = 0; j < menores.length - 1; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    int posGuardada = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = posGuardada;
+                }//Fin if
+            }//Fin for j
+        }//Fin for i
+        menores = numeros;
+        return menores;
+    }//Fin del metodo organizar menores
+
+    public static int[][] MultiplicacionMatrices(int[][] numeros) {
+        
+    }
 }//Fin de la clase
